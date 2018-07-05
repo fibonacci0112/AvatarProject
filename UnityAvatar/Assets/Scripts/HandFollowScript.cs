e8a4b997e25f7432a536f8f1c8bb47c94559298f
Tuan Linh Vu <tuanakki@gmail.com>
2018-07-05 16:25:52 +0200

    Script Tracking Hands

---
+++ UnityAvatar/Assets/Scripts/HandFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandFollowScript : MonoBehaviour {
+
+    public Transform handPosition;
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = handPosition.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = handPosition.transform.position;
+    }
+}
