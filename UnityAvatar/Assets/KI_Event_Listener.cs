09f00c44193903c30c1c5f60338f271f7bb1a081
Unknown <tuanakki@gmail.com>
2018-07-16 16:56:16 +0200

    EventManager (see Description)
    Press t for KI to follow, press z to patrol. We can map it Later to VoiceRecognition

---
+++ UnityAvatar/Assets/KI_Event_Listener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KI_Event_Listener : MonoBehaviour {
+
+    private UnityAction followListener;
+    private UnityAction patrolListener;
+
+    public GameObject KI;
+
+
+    private void Awake()
+    {
+        followListener = new UnityAction(Follow);
+        patrolListener = new UnityAction(Patrol);
+
+        //ki = GameObject.FindGameObjectWithTag("KI");
+    }
+
+   
+
+    void OnEnable()
+    {
+        EventManager.StartListening("KI_follow", followListener);
+        EventManager.StartListening("KI_patrol", patrolListener);
+
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("KI_follow", followListener);
+        EventManager.StopListening("KI_patrol", patrolListener);
+    }
+    private void Follow()
+    {
+       // ki = GameObject.FindGameObjectWithTag("KI");
+        Debug.Log("KI follow activated");
+        KI.GetComponent<NPCSimplePatrol>().enabled = true;
+        KI.GetComponent<Assets.Code.ConnectedPatrol>().enabled = false;
+    }
+
+    private void Patrol()
+    {
+        KI = GameObject.FindGameObjectWithTag("KI");
+        Debug.Log("KI patrol activated");
+        KI.GetComponent<NPCSimplePatrol>().enabled = false;
+        KI.GetComponent<Assets.Code.ConnectedPatrol>().enabled = true;
+    }
+}
---
+++ UnityAvatar/Assets/KI_Event_Trigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KI_Event_Trigger : MonoBehaviour {
+
+	void Update () {
+        if (Input.GetKeyDown("t"))
+        {
+            EventManager.TriggerEvent("KI_follow");
+        }
+
+        if (Input.GetKeyDown("z"))
+        {
+            EventManager.TriggerEvent("KI_patrol");
+        }
+
+
+    }
+}
