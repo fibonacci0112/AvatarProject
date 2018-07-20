9c1a1f49768a91e73e1a91d7a7d5fff0473e9b73
Unknown <tuanakki@gmail.com>
2018-07-20 10:20:05 +0200

    Mergy

---
+++ UnityAvatar/Assets/playMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playMusic : MonoBehaviour {
+
+    public AudioSource pianoSong;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.CompareTag("Player"))
+        {
+            pianoSong.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pianoSong.Stop();
+        }
+    }
+}
